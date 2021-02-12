fcac8b09f294c53ca145bc1ba422dca05b4a6890
William Brito <williamrldb@gmail.com>
2021-02-12 16:20:40 -0400

    --Relacionamento nos models

--- src/database/migrations/1610763209940-CreateUsers.ts
+++ src/database/migrations/1610763209940-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1610763209940 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
