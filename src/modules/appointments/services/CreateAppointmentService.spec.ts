def87511a12cad21c0096ac163e47084628a9a64
William Brito <williamrldb@gmail.com>
2021-04-05 15:17:54 -0400

    --Criando o primeiro teste

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,8 +1,19 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
 describe('CreateAppointment', () => {
-  it('should be able to create a new appointment', () => {
-    expect(1 + 2).toBe(3);
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123456',
+    });
+
+    expect(appointment).toHaveProperty('id');
   });
 
   it('should not be able to create two appointment on the same time', () => {
